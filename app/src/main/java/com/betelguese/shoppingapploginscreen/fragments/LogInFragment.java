d3645ae69a3c4fc03db5233970f8088c0964f030
BluePandora <bluepandora666@gmail.com>
2015-01-28 02:32:22 +0600

    new layout add and some tweeks

--- app/src/main/java/com/betelguese/shoppingapploginscreen/fragments/LogInFragment.java
+++ app/src/main/java/com/betelguese/shoppingapploginscreen/fragments/LogInFragment.java
@@ -25,7 +25,7 @@ public class LogInFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         if (rootView == null) {
-            rootView = inflater.inflate(R.layout.profile_layout, container, false);
+            rootView = inflater.inflate(R.layout.product_detail, container, false);
         }
         return rootView;
     }
