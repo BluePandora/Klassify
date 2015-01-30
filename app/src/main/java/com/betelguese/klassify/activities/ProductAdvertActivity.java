877a69757a31529f36ec657b0458e87f13c69323
BluePandora <bluepandora666@gmail.com>
2015-01-31 01:52:06 +0600

    calender and location image added

---
+++ app/src/main/java/com/betelguese/klassify/activities/ProductAdvertActivity.java
@@ -0,0 +1,9 @@
+package com.betelguese.klassify.activities;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by tuman on 31/1/2015.
+ */
+public class ProductAdvertActivity extends ActionBarActivity {
+}
